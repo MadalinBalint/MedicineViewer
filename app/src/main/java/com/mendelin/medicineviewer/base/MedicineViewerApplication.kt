1bdb023a0e482329959c3153e0619579a78278d8
Madalin Balint <madalin_bacau@yahoo.com>
2021-07-20 11:19:28 +0300

    integrated logging system

--- app/src/main/java/com/mendelin/medicineviewer/base/MedicineViewerApplication.kt
+++ app/src/main/java/com/mendelin/medicineviewer/base/MedicineViewerApplication.kt
@@ -3,6 +3,7 @@ package com.mendelin.medicineviewer.base
 import android.app.Application
 import com.mendelin.medicineviewer.di.appComponents
 import com.mendelin.medicineviewer.di.networkComponent
+import com.mendelin.medicineviewer.logging.TimberPlant
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -10,6 +11,8 @@ import org.koin.core.context.startKoin
 class MedicineViewerApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        TimberPlant.plantTimberDebugLogger()
 
         startKoin {
             androidLogger()
