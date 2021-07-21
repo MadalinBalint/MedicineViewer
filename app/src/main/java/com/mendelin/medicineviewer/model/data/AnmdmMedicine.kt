b86d0d7fa9a157020005194b9f14b8175b1b271c
Madalin Balint <madalin_bacau@yahoo.com>
2021-07-21 08:32:18 +0300

    added models for medicine data

---
+++ app/src/main/java/com/mendelin/medicineviewer/model/data/AnmdmMedicine.kt
@@ -0,0 +1,23 @@
+package com.mendelin.medicineviewer.model.data
+
+data class AnmdmMedicine(
+    var rcp: String,
+    var denumire_com: String,
+    var dci: String,
+    var ff: String,
+    var conc: String,
+    var atc: String,
+    var actiune_terap: String,
+    var rp: String,
+    var ambalaj: String,
+    var volum_ambalaj: String,
+    var valabilitate: String,
+    var cim: String,
+    var app_prod: String,
+    var app_detinator: String,
+    var nr_app: String,
+    var rcp_url: String,
+    var prospect_url: String,
+    var ambalaj_url: String,
+    var url_date: String,
+)
