e095dacb8e7fec8d4e54cbab04dafe7182ff01f4
Madalin Balint <madalin_bacau@yahoo.com>
2021-07-21 09:49:50 +0300

    activity refactoring

--- app/src/main/java/com/mendelin/medicineviewer/view/main/MedicineViewerActivity.kt
+++ app/src/main/java/com/mendelin/medicineviewer/view/main/MedicineViewerActivity.kt
@@ -2,19 +2,26 @@ package com.mendelin.medicineviewer.view.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.mendelin.medicineviewer.R
+import com.mendelin.medicineviewer.databinding.ActivityMedicineviewerBinding
 import com.mendelin.medicineviewer.viewmodel.MedicineViewModel
 import org.koin.android.ext.android.inject
 import timber.log.Timber
 
 
 class MedicineViewerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMedicineviewerBinding
     private val viewModel: MedicineViewModel by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMedicineviewerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        populateUI()
+    }
+
+    private fun populateUI() {
         viewModel.fetchPageInfo()
         viewModel.fetchMedicinesByPage(1)
 
